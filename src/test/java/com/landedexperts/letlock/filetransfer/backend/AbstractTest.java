2f3376c058ed189af37826b9a7ef7fc10c00cdae
Kazem Naderi <knaderi@landedexperts.com>
2019-09-27 16:00:44 -0700

    testing setting env in buildSpec

--- src/test/java/com/landedexperts/letlock/filetransfer/backend/AbstractTest.java
+++ src/test/java/com/landedexperts/letlock/filetransfer/backend/AbstractTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = LetlockFiletransferBackendApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("local")
 public abstract class AbstractTest {
    protected MockMvc mvc;
    @Autowired
