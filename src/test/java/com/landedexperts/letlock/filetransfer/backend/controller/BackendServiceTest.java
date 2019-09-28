b279d3b9ad6ca84feea716fe5ab61861d8babd2a
Kazem Naderi <knaderi@landedexperts.com>
2019-09-27 17:09:02 -0700

    testing setting env in buildSpec

--- src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/BackendServiceTest.java
+++ src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/BackendServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 
 import org.json.JSONObject;
 import org.junit.Before;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.util.TypeKey;
 import com.landedexperts.letlock.filetransfer.backend.AbstractTest;
 
 public class BackendServiceTest extends AbstractTest {
@@ -26,30 +28,41 @@ public class BackendServiceTest extends AbstractTest {
     private static final String TEST_EMAIL = "knaderi@landedexperts.com";
     private static final String TEST_USER_ID = "knaderi12";
     private static ResultMatcher ok = MockMvcResultMatchers.status().isOk();
-    
 
     @Override
     @Before
     public void setUp() {
         super.setUp();
-     }
+    }
 
     @BeforeClass
     public static void setSystemProperty() {
         String hostName = "";
         try {
             hostName = InetAddress.getLocalHost().getCanonicalHostName();
-            System.out.println("*************************************" + hostName);
+            System.out.println("\n************************************* host" + hostName);
+            System.out.println("\n*************************************localHost info" + InetAddress.getLocalHost());
+            System.out.println("\n*************************************System properties" + System.getProperties());
+            Map<String, String> env = System.getenv();
+            for (String name : env.keySet()) {
+                String key = name.toString();
+                String value = env.get(name).toString();
+                System.out.println("\n***********key: " + key + " value: " + value);
+            }
+
+            System.out.println("\n*************************************System properties" + System.getenv());
         } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
-      if(hostName.contains("192.168")) {
+        if (hostName.contains("192.168")) {
             System.out.println("Using local env configuration");
-            System.getProperties().setProperty("spring.profiles.active", "local");            
-       }
-        
+            System.getProperties().setProperty("spring.profiles.active", "local");
+        } else {
+            System.getProperties().setProperty("spring.profiles.active", "dev");
+        }
+
     }
 
     @Test
@@ -76,7 +89,7 @@ public class BackendServiceTest extends AbstractTest {
 
     private String login(String password) throws Exception, UnsupportedEncodingException {
         String uri = "/login";
-                ResultActions resultAction = mvc.perform(MockMvcRequestBuilders.post(uri).param("loginName", TEST_USER_ID)
+        ResultActions resultAction = mvc.perform(MockMvcRequestBuilders.post(uri).param("loginName", TEST_USER_ID)
                 .param("password", password).accept(MediaType.APPLICATION_JSON_VALUE));
 
         resultAction.andExpect(ok);
@@ -145,22 +158,22 @@ public class BackendServiceTest extends AbstractTest {
     }
 
     public String getValuesForGivenKey(String jsonArrayStr, String key) throws Exception {
-        JSONObject jsonObject = new JSONObject(jsonArrayStr);        
-        return  jsonObject.getString(key);
+        JSONObject jsonObject = new JSONObject(jsonArrayStr);
+        return jsonObject.getString(key);
     }
 
     @Test
-    public void updateUserPasswordTest() throws Exception{
-        //change the password to new password and login
+    public void updateUserPasswordTest() throws Exception {
+        // change the password to new password and login
         changePassword(TEST_USER_ID, TEST_PASSWORD, NEW_PASSWORD);
         login(NEW_PASSWORD);
-        //Change the password back and re-login.
+        // Change the password back and re-login.
         changePassword(TEST_USER_ID, NEW_PASSWORD, TEST_PASSWORD);
         login(TEST_PASSWORD);
     }
 
-    private void changePassword(String loginName, String oldPassword, String newPassword) throws Exception{
-        String uri = "/update_user_password"; 
+    private void changePassword(String loginName, String oldPassword, String newPassword) throws Exception {
+        String uri = "/update_user_password";
         ResultActions resultAction = mvc.perform(MockMvcRequestBuilders.post(uri).param("loginName", loginName)
                 .param("oldPassword", oldPassword).param("newPassword", newPassword).accept(MediaType.APPLICATION_JSON_VALUE));
 
