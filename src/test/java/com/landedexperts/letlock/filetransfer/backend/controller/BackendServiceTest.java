e93d39f55e6ebe36cc8ddd9a242d476cbc638ab2
rkaur <rkaur@landedexperts.com>
2019-10-14 19:38:47 -0700

    LL-168  initial saving of the file in S3 bucket

--- src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/BackendServiceTest.java
+++ src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/BackendServiceTest.java
@@ -2,47 +2,57 @@ package com.landedexperts.letlock.filetransfer.backend.controller;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.landedexperts.letlock.filetransfer.backend.AbstractTest;
 
 public class BackendServiceTest extends AbstractTest {
 
-    private static final String TEST_PASSWORD = "passw0rd!";
-    private static final String NEW_PASSWORD = "newpassw0rd!";
-    private static final String TEST_EMAIL = "knaderi@landedexperts.com";
-    private static final String TEST_USER_ID = "knaderi12";
-    private static ResultMatcher ok = MockMvcResultMatchers.status().isOk();
+	private static final String TEST_PASSWORD = "passw0rd!";
+	private static final String NEW_PASSWORD = "newpassw0rd!";
+	private static final String TEST_EMAIL = "knaderi@landedexperts.com";
+	private static final String TEST_USER_ID = "knaderi12";
+	private static ResultMatcher ok = MockMvcResultMatchers.status().isOk();
+    @Value("${s3.storage.bucket}")
+    private String s3Bucket;
+    
+    @Autowired
+    private FileController fileController;
 
-    @Override
-    @Before
-    public void setUp() {
-        super.setUp();
-    }
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
 
-    @BeforeClass
-    public static void setSystemProperty() {
-        String activeProfile = "dev";
-        String mvnCommandLineArgs = System.getenv().get("MAVEN_CMD_LINE_ARGS");
-        if (!StringUtils.isEmpty(mvnCommandLineArgs)) {
-            int index = mvnCommandLineArgs.indexOf("-Dspring.profiles.active=");
-            activeProfile = mvnCommandLineArgs.substring(index + 25);
-        }
-        System.getProperties().setProperty("spring.profiles.active", activeProfile);
+	@BeforeClass
+	public static void setSystemProperty() {
+		String activeProfile = "dev";
+		String mvnCommandLineArgs = System.getenv().get("MAVEN_CMD_LINE_ARGS");
+		if (!StringUtils.isEmpty(mvnCommandLineArgs)) {
+			int index = mvnCommandLineArgs.indexOf("-Dspring.profiles.active=");
+			activeProfile = mvnCommandLineArgs.substring(index + 25);
+		}
+		System.getProperties().setProperty("spring.profiles.active", activeProfile);
 
-    }
+	}
 
     @Test
     public void registerTest() throws Exception {
@@ -163,4 +173,15 @@ public class BackendServiceTest extends AbstractTest {
         assertTrue(content.length() > 0);
         assertTrue(content.contains("\"result\":true"));
     }
+
+	@Test
+	public void testSaveFileOnDisk() throws Exception {
+		MultipartFile localFile = new MockMultipartFile("testSaveFile.txt", "This is a test file".getBytes());
+		String localFilePath = System.getProperty("user.home") + File.separator + "testSaveFile.txt";
+		String remoteFilePath = ".";
+		//localFile.transferTo(new File(localFilePath));
+		fileController.saveFileOnDisk(localFile, localFilePath);
+		fileController.uploadFileToRemote(localFilePath, remoteFilePath);	
+	}
+	
 }
