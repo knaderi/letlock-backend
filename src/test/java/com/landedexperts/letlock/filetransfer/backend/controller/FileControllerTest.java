a297c707119c9836a8e7c97c76f1712ba2519908
Kazem Naderi <knaderi@landedexperts.com>
2019-10-15 13:27:12 -0700

    ::-168 improving remote file uplodad and download

--- src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/FileControllerTest.java
+++ src/test/java/com/landedexperts/letlock/filetransfer/backend/controller/FileControllerTest.java
@@ -8,20 +8,22 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.landedexperts.letlock.filetransfer.backend.AbstractTest;
 
 public class FileControllerTest extends AbstractTest{
     
+    private static final String TEST_FILE_NAME = "testSaveFile.txt";
+
+    private static final String TEST_FILE_CONTENT = "This is a test file";
+
     @Value("${s3.storage.bucket}")
     private String s3Bucket;
     
@@ -34,22 +36,11 @@ public class FileControllerTest extends AbstractTest{
         super.setUp();
     }
 
-    @BeforeClass
-    public static void setSystemProperty() {
-        String activeProfile = "dev";
-        String mvnCommandLineArgs = System.getenv().get("MAVEN_CMD_LINE_ARGS");
-        if (!StringUtils.isEmpty(mvnCommandLineArgs)) {
-            int index = mvnCommandLineArgs.indexOf("-Dspring.profiles.active=");
-            activeProfile = mvnCommandLineArgs.substring(index + 25);
-        }
-        System.getProperties().setProperty("spring.profiles.active", activeProfile);
-
-    }
     
     @Test
-    public void testSaveFileOnDisk() throws Exception {
-        MultipartFile localFile = new MockMultipartFile("testSaveFile.txt", "This is a test file".getBytes());
-        String localFilePath = System.getProperty("user.home") + File.separator + "testSaveFile.txt";
+    public void testUploadFile() throws Exception {
+        MultipartFile localFile = new MockMultipartFile(TEST_FILE_NAME, TEST_FILE_CONTENT.getBytes());
+        String localFilePath = System.getProperty("user.home") + File.separator + TEST_FILE_NAME;
         String remoteFilePath = ".";
         fileController.saveFileOnDisk(localFile, localFilePath);
         fileController.uploadFileToRemote(localFilePath, remoteFilePath);   
@@ -58,7 +49,7 @@ public class FileControllerTest extends AbstractTest{
     @Test
     public void testDownloadFile() throws Exception {
  
-        ResponseEntity<Resource>  downloadInputStream= fileController.downloadRemoteFile("testSaveFile.txt");
+        ResponseEntity<Resource>  downloadInputStream= fileController.downloadRemoteFile(TEST_FILE_NAME);
         ByteArrayOutputStream result = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int length;
@@ -66,7 +57,7 @@ public class FileControllerTest extends AbstractTest{
         while ((length = inputStream.read(buffer)) != -1) {
             result.write(buffer, 0, length);
         }
-        assertEquals("This is a test file", result.toString(StandardCharsets.UTF_8.name()));        
+        assertEquals(TEST_FILE_CONTENT, result.toString(StandardCharsets.UTF_8.name()));        
     }
 
 }
