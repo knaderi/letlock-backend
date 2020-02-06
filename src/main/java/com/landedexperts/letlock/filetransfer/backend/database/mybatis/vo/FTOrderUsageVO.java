967801c70a696c399fe50c121516f9259b0b3dd3
rkaur <rkaur@landedexperts.com>
2020-02-06 14:06:56 -0800

    Added nwe value object  classes and response that can be used by order usage api. Also added Test utils for creating mock wallet addess and paypal transactions

---
+++ src/main/java/com/landedexperts/letlock/filetransfer/backend/database/mybatis/vo/FTOrderUsageVO.java
@@ -0,0 +1,125 @@
+package com.landedexperts.letlock.filetransfer.backend.database.mybatis.vo;
+
+import java.util.UUID;
+
+public class FTOrderUsageVO {
+    private UUID uuid;
+    private long order_line_item_id;
+    private long sender_id;
+    private String sender_login_name;
+    private long receiver_id;
+    private String receiver_login_name;
+    private short credit_used;
+    private String status;
+    private String transfer_dt;
+    private String smart_contract_address;
+    private String is_active;
+    private int package_id;
+    private String package_name;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+
+    public long getOrderLineItemId() {
+        return order_line_item_id;
+    }
+
+    public void setOrderLineItemId(long orderLineItemId) {
+        this.order_line_item_id = orderLineItemId;
+    }
+
+    public long getSenderId() {
+        return sender_id;
+    }
+
+    public void setSenderId(long senderId) {
+        this.sender_id = senderId;
+    }
+
+    public String getSenderLoginName() {
+        return sender_login_name;
+    }
+
+    public void setSenderLoginName(String senderLoginName) {
+        this.sender_login_name = senderLoginName;
+    }
+
+    public long getReceiverId() {
+        return receiver_id;
+    }
+
+    public void setReceiverId(long receiverId) {
+        this.receiver_id = receiverId;
+    }
+
+    public String getReceiverLoginName() {
+        return receiver_login_name;
+    }
+
+    public void setReceiverLoginName(String receiverLoginName) {
+        this.receiver_login_name = receiverLoginName;
+    }
+
+    public short getCreditUsed() {
+        return credit_used;
+    }
+
+    public void setCreditUsed(short creditUsed) {
+        this.credit_used = creditUsed;
+    }
+
+    public String getfTStatus() {
+        return status;
+    }
+
+    public void setfTStatus(String fTStatus) {
+        this.status = fTStatus;
+    }
+
+    public String getFtDate() {
+        return transfer_dt;
+    }
+
+    public void setFtDate(String ftDate) {
+        this.transfer_dt = ftDate;
+    }
+
+    public String getContractAddress() {
+        return smart_contract_address;
+    }
+
+    public void setContractAddress(String contractAddress) {
+        this.smart_contract_address = contractAddress;
+    }
+
+    public String getIsActive() {
+        return is_active;
+    }
+
+    public void setIsActive(String isActive) {
+        this.is_active = isActive;
+    }
+
+    public int getPackageId() {
+        return package_id;
+    }
+
+    public void setPackageId(int packageId) {
+        this.package_id = packageId;
+    }
+
+    public String getPackageName() {
+        return package_name;
+    }
+
+    public void setPackageName(String packageName) {
+        this.package_name = packageName;
+    }
+
+}
---
+++ src/test/java/com/landedexperts/letlock/filetransfer/backend/TestUtils.java
@@ -0,0 +1,62 @@
+package com.landedexperts.letlock.filetransfer.backend;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public  final class TestUtils {
+    
+
+    public static String createWalletAddress() {
+
+        int number = 40;
+        char[] charArr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };// hex digits array
+
+        Random rand = new Random();
+        String result = "";
+        for (int x = 0; x < number; x++) {
+            int resInt = rand.nextInt(charArr.length);// random array element
+            result += charArr[resInt];
+        }
+        return result;
+    }
+    
+    public static String createPayPalTransactionId() {
+
+        int number = 13;
+        char[] charArr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };// hex digits array
+
+        Random rand = new Random();
+        String result = "";
+        for (int x = 0; x < number; x++) {
+            int resInt = rand.nextInt(charArr.length);// random array element
+            result += charArr[resInt];
+        }
+        return result;
+    }
+    
+    
+    public static int getNumberOfRepetitions(String content, String repeatingString) {
+           return getNumberOfRepetitions(content, repeatingString, 0,0);
+    }
+    
+    public static int getNumberOfRepetitions(String content, String repeatingString, int count, int startingIndex) {
+        int stringRepeatedLocationIndex = content.indexOf(repeatingString);
+        if(stringRepeatedLocationIndex != -1) {
+           int newStartingIndex = stringRepeatedLocationIndex + repeatingString.length();
+            count++;
+            String newContent = content.substring(newStartingIndex);
+            count = getNumberOfRepetitions(newContent, repeatingString,count, newStartingIndex );
+        }
+        return count;
+    }
+    
+    @Test
+    public void testCount() {
+        String content = "{\"returnCode\":\"SUCCESS\",\"returnMessage\":\"\",\"result\":[{\"uuid\":null,\"packageName\":\"Twenty-Transfer\",\"orderLineItemId\":20,\"receiverLoginName\":\"Bob1111\",\"contractAddress\":null,\"senderLoginName\":\"WestonDonnelly\",\"senderId\":23,\"receiverId\":2,\"ftDate\":\"2020-02-05 16:18:23.590946\",\"packageId\":3,\"isActive\":\"t\",\"creditUsed\":1,\"fTStatus\":\"Waiting for Recipient\"},{\"uuid\":null,\"packageName\":\"Twenty-Transfer\",\"orderLineItemId\":20,\"receiverLoginName\":\"Bob1111\",\"contractAddress\":null,\"senderLoginName\":\"WestonDonnelly\",\"senderId\":23,\"receiverId\":2,\"ftDate\":\"2020-02-05 16:18:23.420503\",\"packageId\":3,\"isActive\":\"t\",\"creditUsed\":1,\"fTStatus\":\"Waiting for Recipient\"}]}";
+        assertEquals(2,getNumberOfRepetitions(content, "\"creditUsed\":1"));
+    }
+
+}
