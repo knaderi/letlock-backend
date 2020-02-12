2294e98d480c5d5c141ad1f88682ddb1d255a0ac
Kazem Naderi <knaderi@landedexperts.com>
2020-02-11 17:45:40 -0800

    updating the code to use db-gateway

--- src/main/java/com/landedexperts/letlock/filetransfer/backend/blockchain/gateway/impl/DBGatewayService.java
+++ src/main/java/com/landedexperts/letlock/filetransfer/backend/blockchain/gateway/impl/DBGatewayService.java
@@ -1,5 +1,6 @@
 package com.landedexperts.letlock.filetransfer.backend.blockchain.gateway.impl;
 
+import java.util.Random;
 import java.util.UUID;
 
 import org.slf4j.Logger;
@@ -19,8 +20,8 @@ public class DBGatewayService extends BlockChainGatewayService {
     FileTransferMapper fileTransferMapper;
     private final Logger logger = LoggerFactory.getLogger(DBGatewayService.class);
     
-    private String senderWalletAddress;
-    private String receiverWalletAddress;
+    private String senderWalletAddress = createWalletAddress();
+    private String receiverWalletAddress = createWalletAddress();
 
     public DBGatewayService() {
         super();
@@ -34,6 +35,7 @@ public class DBGatewayService extends BlockChainGatewayService {
         // call below:
         // web3.eth.accounts.recoverTransaction(transaction)
 //        return transaction;
+        
     }
 
     private String getWalletAddressForStep(String step) {
@@ -62,13 +64,9 @@ public class DBGatewayService extends BlockChainGatewayService {
 //        " $1," +
 //        " DECODE( $2, 'hex' )" +
 //        " )",
-        this.senderWalletAddress = senderAddress.substring(0, 2).equals("0x") ? senderAddress.substring(2)
-                : senderAddress;
-        this.receiverWalletAddress = receiverAddress.substring(0, 2).equals("0x") ? receiverAddress.substring(2)
-                : receiverAddress;
         String smartContractAddres = receiverWalletAddress;
         ReturnCodeMessageResponse answer = fileTransferMapper.fileTransferSetContractAddress(fileTransferUuid, smartContractAddres);
-        fileTransferMapper.fileTransferSetTransferStepCompleted(fileTransferUuid, receiverWalletAddress, "step_1_rec_pubkey", "94E6d91C51b44cCAF97fE69dD122968eE8672173");
+        fileTransferMapper.fileTransferSetTransferStepCompleted(fileTransferUuid, receiverWalletAddress, "step_1_rec_pubkey", createTransactionHash());
        // fileTransferMapper.fileTransferSetTransferStepCompleted(fileTransferUuid, senderWalletAddress, "step_2_send_doc_info", "94E6d91C51b44cCAF97fE69dD122968eE8672173");
         // BooleanVO returnValue =
         // fileTransferMapper.fileTransferSetContractAddress(fileTransferUuid,
@@ -86,7 +84,25 @@ public class DBGatewayService extends BlockChainGatewayService {
     @Override
     public String fund(UUID fileTransferUuid, String signedTransactionHex, String step) throws Exception {
         fileTransferMapper.fileTransferSetTransferStepCompleted(fileTransferUuid, getWalletAddressForStep(step), step, signedTransactionHex);
-        return signedTransactionHex + "zzz";
+        return signedTransactionHex;
+    }
+    
+    public  String createWalletAddress() {
+
+        int number = 40;
+        char[] charArr = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };// hex digits array
+
+        Random rand = new Random();
+        String result = "";
+        for (int x = 0; x < number; x++) {
+            int resInt = rand.nextInt(charArr.length);// random array element
+            result += charArr[resInt];
+        }
+        return result;
+    }
+    
+    public String createTransactionHash() {
+        return createWalletAddress();
     }
 
 //    client.query(
