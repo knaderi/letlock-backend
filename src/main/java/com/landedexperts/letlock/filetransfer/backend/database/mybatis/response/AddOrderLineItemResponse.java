d6fbe9e3cb59221a8ca889ce94cda58c93860948
rkaur <rkaur@landedexperts.com>
2020-02-29 15:33:24 -0800

    LLW-126 added copy right text to all source files

--- src/main/java/com/landedexperts/letlock/filetransfer/backend/database/mybatis/response/AddOrderLineItemResponse.java
+++ src/main/java/com/landedexperts/letlock/filetransfer/backend/database/mybatis/response/AddOrderLineItemResponse.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (C) Landed Experts Technologies Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Kazem Naderi - 2019
+ ******************************************************************************/
 package com.landedexperts.letlock.filetransfer.backend.database.mybatis.response;
 
 public class AddOrderLineItemResponse extends ReturnCodeMessageResponse {
