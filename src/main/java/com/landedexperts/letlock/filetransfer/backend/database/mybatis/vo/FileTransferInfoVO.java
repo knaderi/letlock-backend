e8ed44c3c5d751d3dc37ddc234df58c81deb9fcf
Kazem Naderi <knaderi@landedexperts.com>
2020-08-04 16:25:19 -0700

    ll-308 adding new field to filetransfer to capture the info on whther the sender or reciever cancels a file-transfer.

--- src/main/java/com/landedexperts/letlock/filetransfer/backend/database/mybatis/vo/FileTransferInfoVO.java
+++ src/main/java/com/landedexperts/letlock/filetransfer/backend/database/mybatis/vo/FileTransferInfoVO.java
@@ -11,145 +11,149 @@ import java.util.Date;
 import com.landedexperts.letlock.filetransfer.backend.database.mybatis.response.ReturnCodeMessageResponse;
 
 public class FileTransferInfoVO extends ReturnCodeMessageResponse {
-	FileTransferInfoRecordVO fileTransferInfoRecordVO = new FileTransferInfoRecordVO();
+    FileTransferInfoRecordVO fileTransferInfoRecordVO = new FileTransferInfoRecordVO();
 
-	public FileTransferInfoRecordVO getFileTransferInfoRecord() {
-		return fileTransferInfoRecordVO;
-	}
+    public FileTransferInfoRecordVO getFileTransferInfoRecord() {
+        return fileTransferInfoRecordVO;
+    }
 
-	public String getFileTransferUuid() {
-		return fileTransferInfoRecordVO.getFileTransferUuid();
-	}
+    public String getFileTransferUuid() {
+        return fileTransferInfoRecordVO.getFileTransferUuid();
+    }
 
-	public void setFileTransferUuid(String fileTransferUuid) {
-		fileTransferInfoRecordVO.setFileTransferUuid(fileTransferUuid);
-	}
-	
-	public String getSenderLoginName() {
-		return fileTransferInfoRecordVO.getSenderLoginName();
-	}
+    public void setFileTransferUuid(String fileTransferUuid) {
+        fileTransferInfoRecordVO.setFileTransferUuid(fileTransferUuid);
+    }
 
-	public void setSenderLoginName(String senderLoginName) {
-		fileTransferInfoRecordVO.setSenderLoginName(senderLoginName);
-	}
-	
-	public String getSenderWalletAddressUuid() {
-		return fileTransferInfoRecordVO.getSenderWalletAddressUuid();
-	}
+    public String getSenderLoginName() {
+        return fileTransferInfoRecordVO.getSenderLoginName();
+    }
 
-	public void setSenderWalletAddressUuid(String senderWalletAddressUuid) {
-		fileTransferInfoRecordVO.setSenderWalletAddressUuid(senderWalletAddressUuid);
-	}
+    public void setSenderLoginName(String senderLoginName) {
+        fileTransferInfoRecordVO.setSenderLoginName(senderLoginName);
+    }
 
-	public String getSenderWalletAddress() {
-		return fileTransferInfoRecordVO.getSenderWalletAddress();
-	}
+    public String getSenderWalletAddressUuid() {
+        return fileTransferInfoRecordVO.getSenderWalletAddressUuid();
+    }
 
-	public void setSenderWalletAddress(String senderWalletAddress) {
-		fileTransferInfoRecordVO.setSenderWalletAddress(senderWalletAddress);
-	}
+    public void setSenderWalletAddressUuid(String senderWalletAddressUuid) {
+        fileTransferInfoRecordVO.setSenderWalletAddressUuid(senderWalletAddressUuid);
+    }
 
-	public String getReceiverLoginName() {
-		return fileTransferInfoRecordVO.getReceiverLoginName();
-	}
+    public String getSenderWalletAddress() {
+        return fileTransferInfoRecordVO.getSenderWalletAddress();
+    }
 
-	public void setReceiverLoginName(String receiverLoginName) {
-		fileTransferInfoRecordVO.setReceiverLoginName(receiverLoginName);
-	}
+    public void setSenderWalletAddress(String senderWalletAddress) {
+        fileTransferInfoRecordVO.setSenderWalletAddress(senderWalletAddress);
+    }
 
-	public String getReceiverWalletAddressUuid() {
-		return fileTransferInfoRecordVO.getReceiverWalletAddressUuid();
-	}
+    public String getReceiverLoginName() {
+        return fileTransferInfoRecordVO.getReceiverLoginName();
+    }
 
-	public void setReceiverWalletAddressUuid(String receiverWalletAddressUuid) {
-		fileTransferInfoRecordVO.setReceiverWalletAddressUuid(receiverWalletAddressUuid);
-	}
+    public void setReceiverLoginName(String receiverLoginName) {
+        fileTransferInfoRecordVO.setReceiverLoginName(receiverLoginName);
+    }
 
-	public String getReceiverWalletAddress() {
-		return fileTransferInfoRecordVO.getReceiverWalletAddress();
-	}
+    public String getReceiverWalletAddressUuid() {
+        return fileTransferInfoRecordVO.getReceiverWalletAddressUuid();
+    }
 
-	public void setReceiverWalletAddress(String receiverWalletAddress) {
-		fileTransferInfoRecordVO.setReceiverWalletAddress(receiverWalletAddress);
-	}
+    public void setReceiverWalletAddressUuid(String receiverWalletAddressUuid) {
+        fileTransferInfoRecordVO.setReceiverWalletAddressUuid(receiverWalletAddressUuid);
+    }
 
-	public String getSmartContractAddress() {
-		return fileTransferInfoRecordVO.getSmartContractAddress();
-	}
+    public String getReceiverWalletAddress() {
+        return fileTransferInfoRecordVO.getReceiverWalletAddress();
+    }
 
-	public void setSmartContractAddress(String smartContractAddress) {
-		fileTransferInfoRecordVO.setSmartContractAddress(smartContractAddress);
-	}
+    public void setReceiverWalletAddress(String receiverWalletAddress) {
+        fileTransferInfoRecordVO.setReceiverWalletAddress(receiverWalletAddress);
+    }
 
-	public String getFunding1RecPubkeyStatus() {
-		return fileTransferInfoRecordVO.getFunding1RecPubkeyStatus();
-	}
+    public String getSmartContractAddress() {
+        return fileTransferInfoRecordVO.getSmartContractAddress();
+    }
 
-	public void setFunding1RecPubkeyStatus(String funding1RecPubkeyStatus) {
-		fileTransferInfoRecordVO.setFunding1RecPubkeyStatus(funding1RecPubkeyStatus);
-	}
+    public void setSmartContractAddress(String smartContractAddress) {
+        fileTransferInfoRecordVO.setSmartContractAddress(smartContractAddress);
+    }
 
-	public String getFunding1RecPubkeyTransactionHash() {
-		return fileTransferInfoRecordVO.getFunding1RecPubkeyTransactionHash();
-	}
+    public String getFunding1RecPubkeyStatus() {
+        return fileTransferInfoRecordVO.getFunding1RecPubkeyStatus();
+    }
 
-	public void setFunding1RecPubkeyTransactionHash(String funding1RecPubkeyTransactionHash) {
-		fileTransferInfoRecordVO.setFunding1RecPubkeyTransactionHash(funding1RecPubkeyTransactionHash);
-	}
+    public void setFunding1RecPubkeyStatus(String funding1RecPubkeyStatus) {
+        fileTransferInfoRecordVO.setFunding1RecPubkeyStatus(funding1RecPubkeyStatus);
+    }
 
-	public String getFunding2SendDocinfoStatus() {
-		return fileTransferInfoRecordVO.getFunding2SendDocinfoStatus();
-	}
+    public String getFunding1RecPubkeyTransactionHash() {
+        return fileTransferInfoRecordVO.getFunding1RecPubkeyTransactionHash();
+    }
 
-	public void setFunding2SendDocinfoStatus(String funding2SendDocinfoStatus) {
-		fileTransferInfoRecordVO.setFunding2SendDocinfoStatus(funding2SendDocinfoStatus);
-	}
+    public void setFunding1RecPubkeyTransactionHash(String funding1RecPubkeyTransactionHash) {
+        fileTransferInfoRecordVO.setFunding1RecPubkeyTransactionHash(funding1RecPubkeyTransactionHash);
+    }
 
-	public String getFunding2SendDocinfoTransactionHash() {
-		return fileTransferInfoRecordVO.getFunding2SendDocinfoTransactionHash();
-	}
+    public String getFunding2SendDocinfoStatus() {
+        return fileTransferInfoRecordVO.getFunding2SendDocinfoStatus();
+    }
 
-	public void setFunding2SendDocinfoTransactionHash(String funding2SendDocinfoTransactionHash) {
-		fileTransferInfoRecordVO.setFunding2SendDocinfoTransactionHash(funding2SendDocinfoTransactionHash);
-	}
+    public void setFunding2SendDocinfoStatus(String funding2SendDocinfoStatus) {
+        fileTransferInfoRecordVO.setFunding2SendDocinfoStatus(funding2SendDocinfoStatus);
+    }
 
-	public String getFunding3RecFinalStatus() {
-		return fileTransferInfoRecordVO.getFunding3RecFinalStatus();
-	}
+    public String getFunding2SendDocinfoTransactionHash() {
+        return fileTransferInfoRecordVO.getFunding2SendDocinfoTransactionHash();
+    }
 
-	public void setFunding3RecFinalStatus(String funding3RecFinalStatus) {
-		fileTransferInfoRecordVO.setFunding3RecFinalStatus(funding3RecFinalStatus);
-	}
+    public void setFunding2SendDocinfoTransactionHash(String funding2SendDocinfoTransactionHash) {
+        fileTransferInfoRecordVO.setFunding2SendDocinfoTransactionHash(funding2SendDocinfoTransactionHash);
+    }
 
-	public String getFunding3RecFinalTransactionHash() {
-		return fileTransferInfoRecordVO.getFunding3RecFinalTransactionHash();
-	}
+    public String getFunding3RecFinalStatus() {
+        return fileTransferInfoRecordVO.getFunding3RecFinalStatus();
+    }
 
-	public void setFunding3RecFinalTransactionHash(String funding3RecFinalTransactionHash) {
-		fileTransferInfoRecordVO.setFunding3RecFinalTransactionHash(funding3RecFinalTransactionHash);
-	}
+    public void setFunding3RecFinalStatus(String funding3RecFinalStatus) {
+        fileTransferInfoRecordVO.setFunding3RecFinalStatus(funding3RecFinalStatus);
+    }
 
-	public boolean isFileTransferIsActive() {
-		return fileTransferInfoRecordVO.isFileTransferIsActive();
-	}
+    public String getFunding3RecFinalTransactionHash() {
+        return fileTransferInfoRecordVO.getFunding3RecFinalTransactionHash();
+    }
 
-	public void setFileTransferIsActive(boolean fileTransferIsActive) {
-		fileTransferInfoRecordVO.setFileTransferIsActive(fileTransferIsActive);
-	}
+    public void setFunding3RecFinalTransactionHash(String funding3RecFinalTransactionHash) {
+        fileTransferInfoRecordVO.setFunding3RecFinalTransactionHash(funding3RecFinalTransactionHash);
+    }
 
-	public Date getFileTransferCreate() {
-		return fileTransferInfoRecordVO.getFileTransferCreate();
-	}
+    public String getFileTransferActiveCode() {
+        return fileTransferInfoRecordVO.getFileTransferActiveCode();
+    }
 
-	public void setFileTransferCreate(Date fileTransferCreate) {
-		fileTransferInfoRecordVO.setFileTransferCreate(fileTransferCreate);
-	}
+    public void setFileTransferActiveCode(String fileTransferActiveCode) {
+        fileTransferInfoRecordVO.setFileTransferActiveCode(fileTransferActiveCode);
+    }
 
-	public Date getFileTransferUpdate() {
-		return fileTransferInfoRecordVO.getFileTransferUpdate();
-	}
+    public boolean isFileTransferActive() {
+        return fileTransferInfoRecordVO.isFileTransferActive();
+    }
 
-	public void setFileTransferUpdate(Date fileTransferUpdate) {
-		fileTransferInfoRecordVO.setFileTransferUpdate(fileTransferUpdate);
-	}
+    public Date getFileTransferCreate() {
+        return fileTransferInfoRecordVO.getFileTransferCreate();
+    }
+
+    public void setFileTransferCreate(Date fileTransferCreate) {
+        fileTransferInfoRecordVO.setFileTransferCreate(fileTransferCreate);
+    }
+
+    public Date getFileTransferUpdate() {
+        return fileTransferInfoRecordVO.getFileTransferUpdate();
+    }
+
+    public void setFileTransferUpdate(Date fileTransferUpdate) {
+        fileTransferInfoRecordVO.setFileTransferUpdate(fileTransferUpdate);
+    }
 }
